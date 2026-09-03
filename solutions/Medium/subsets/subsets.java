031b33e360bdbedf9fe0a3225450687552011546
Jaibhev Dhiman <legendaryjd30@gmail.com>
2026-09-03 14:03:25 +0530

    Update solution: Subsets

--- solutions/Medium/subsets/subsets.java
+++ solutions/Medium/subsets/subsets.java
@@ -8,7 +8,6 @@ class Solution {
         }
 
         for (int i = index; i < arr.length; i++) {
-            if (i > index && arr[i] == arr[i - 1]) continue;
             lst.add(arr[i]);
             sequence(i + 1, lst, arr);
             lst.remove(lst.size() - 1);
