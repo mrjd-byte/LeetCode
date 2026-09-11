f36067f15e6062d2aab80f01c5f89043999c38fb
Jaibhev Dhiman <legendaryjd30@gmail.com>
2026-09-11 20:08:59 +0530

    Add solution: Sum of Subarray Ranges

---
+++ solutions/Medium/sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int smallest = nums[i];
+            int largest = nums[i];
+
+            for (int j = i; j < n; j++) {
+                smallest = Math.min(smallest, nums[j]);
+                largest = Math.max(largest, nums[j]);
+                sum += (largest - smallest);
+            }
+        }
+
+        return sum;
+    }
+}
