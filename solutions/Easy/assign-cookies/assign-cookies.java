0de04fe7145bf713cded242a451d51c6b5f8e091
Jaibhev Dhiman <legendaryjd30@gmail.com>
2026-09-22 20:09:25 +0530

    Add solution: Assign Cookies

---
+++ solutions/Easy/assign-cookies/assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int left = 0;
+        int right = 0;
+
+        while (left < s.length && right < g.length) {
+            if (g[right] <= s[left]) {
+                right++;
+            }
+            left++;
+        }
+        return right;
+    }
+}
